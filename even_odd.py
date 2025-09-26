05ca7cafd608bc5f36b144c8ed7d0eeee400d142
Siddhesh Alhat <siddesh5432alhat@gmail.com>
2025-09-26 23:59:54 +0530

    first commit

---
+++ even_odd.py
@@ -0,0 +1,11 @@
+try:
+    num = int(input("Enter an integer: "))
+
+
+    if num % 2 == 0:
+        print(f"{num} is Even.")
+    else:
+        print(f"{num} is Odd.")
+
+except ValueError:
+    print("Invalid input! Please enter an integer.")
